91e6b00fe8fc05ffa8bb811db7c0cf8338f30458
Calvin Laurenson <89622328+calvin-laurenson@users.noreply.github.com>
2022-02-09 13:14:20 -0800

    fixed bg and started ui

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ interface Drawable {
 
 class Background {
   sprite: Sprite;
-
+  bgId: string;
+  parsedBgLoc: Chunk;
   constructor(bgId: string) {
+    this.bgId = bgId;
+    this.parsedBgLoc = parseBgId(bgId);
     this.sprite = createSprite(200, 200);
     this.sprite.setAnimation(bgId);
     this.sprite.visible = false;
@@ -73,8 +76,53 @@ function globalToLocal(pos: Point): Point {
   return { x: remainingPosDecimal.x * 400, y: remainingPosDecimal.y * 400 };
 }
 
+function parseBgId(bgId: string): Chunk {
+  try {
+    const parsed = bgId.split("_");
+    return { c: Number(parsed[0]), r: Number(parsed[1]) };
+  } catch (e) {
+    throw new Error(`Invalid background id: ${bgId}`);
+  }
+}
+function stringifyBgId(chunk: Chunk): string {
+  return `${chunk.c}_${chunk.r}`;
+}
+
+class UIWidget implements Drawable {
+  draw() {
+      
+  }
+}
+
+class UIScreen implements Drawable {
+  widgets: UIWidget[] = [];
+  constructor() {
+    this.widgets.push(new UIWidget());
+  }
+
+  draw() {
+    this.widgets.forEach((widget) => {
+      widget.draw();
+    });
+  }
+}
+
+class UIManager implements Drawable {
+  screen: UIScreen | null = null;
+
+
+  draw() {
+      if(this.screen){
+        this.screen.draw();
+      }
+  }
+}
+
+
 class BackgroundManager implements Drawable {
   readonly speed: number = 5;
+
+  readonly maxArea: Point = { x: 0, y: 0 };
 
   loc: Chunk;
 
@@ -82,51 +130,53 @@ class BackgroundManager implements Drawable {
 
   pos: Point = { x: 200, y: 200 };
 
-  toHide: Sprite[] = []
+  toHide: Sprite[] = [];
 
   constructor(startingLoc: string, bgs: string[]) {
     for (let i = 0; i < bgs.length; i++) {
       const e = bgs[i];
       const bg = new Background(e);
       this.bgs[e] = bg;
+      if (bg.parsedBgLoc.c * 400 > this.maxArea.x) {
+        this.maxArea.x = bg.parsedBgLoc.c * 400;
+      }
+      if (bg.parsedBgLoc.r * 400 > this.maxArea.y) {
+        this.maxArea.y = bg.parsedBgLoc.r * 400;
+      }
     }
-    this.loc = this.parseBgId(startingLoc);
+    console.log(JSON.stringify(this.maxArea));
 
-   
+    this.loc = parseBgId(startingLoc);
   }
   draw() {
-    this.toHide.forEach(v => v.visible = false)
-    this.toHide = []
+    this.toHide.forEach((v) => (v.visible = false));
+    this.toHide = [];
     background("white");
     if (keyDown("up")) {
-      this.pos.y -= this.speed;
+      if (this.pos.y - this.speed > 0) {
+        this.pos.y -= this.speed;
+      }
     }
     if (keyDown("down")) {
-      this.pos.y += this.speed;
+      if (this.pos.y + this.speed < this.maxArea.y) {
+        this.pos.y += this.speed;
+      }
     }
     if (keyDown("left")) {
-      this.pos.x -= this.speed;
+      if (this.pos.x - this.speed > 0) {
+        this.pos.x -= this.speed;
+      }
     }
     if (keyDown("right")) {
-      this.pos.x += this.speed;
+      if (this.pos.x + this.speed < this.maxArea.x) {
+        this.pos.x += this.speed;
+      }
     }
     // this.bgs["1-1"].sprite.x = -this.pos.x + 400;
     // this.bgs["1-1"].sprite.y = -this.pos.y + 400;
 
     this.moveBackgrounds();
     // console.log(JSON.stringify(viewableBgs.map(v => this.stringifyBgId(v))));
-  }
-
-  private parseBgId(bgId: string): Chunk {
-    try {
-      const parsed = bgId.split("_");
-      return { c: Number(parsed[0]), r: Number(parsed[1]) };
-    } catch (e) {
-      throw new Error(`Invalid background id: ${bgId}`);
-    }
-  }
-  stringifyBgId(chunk: Chunk): string {
-    return `${chunk.c}_${chunk.r}`;
   }
 
   getViewableBgs(pos: Point): Chunk[] {
@@ -170,7 +220,7 @@ class BackgroundManager implements Drawable {
 
   moveBackgrounds() {
     const pos = this.pos;
-    const localPos = globalToLocal(pos)
+    const localPos = globalToLocal(pos);
     const viewableBgs = this.getViewableBgs(pos);
 
     const currentChunk = globalToChunk(pos);
@@ -178,25 +228,24 @@ class BackgroundManager implements Drawable {
     //console.log(JSON.stringify(pos))
     //console.log(JSON.stringify(viewableBgs))
     //console.log(Object.keys(this.bgs))
-    console.log(viewableBgs.map((v) => this.stringifyBgId(v))); 
+    // console.log(viewableBgs.map((v) => this.stringifyBgId(v)));
 
     viewableBgs.forEach((v) => {
       const diff = this.subtractChunks(v, currentChunk);
-      const bgId = this.stringifyBgId(v);
+      const bgId = stringifyBgId(v);
       if (bgId in this.bgs) {
         // console.log("Moving bg " + bgId);
-        
+
         const bg = this.bgs[bgId];
         this.toHide.push(bg.sprite);
-        bg.sprite.visible = true
+        bg.sprite.visible = true;
 
-        bg.sprite.x = ((localPos.x) + (400 * diff.c));
-        bg.sprite.y = ((localPos.y) + (400 * diff.r));  
+        bg.sprite.x = 400 - localPos.x + (400 * diff.c);
+        bg.sprite.y = 400 - localPos.y + (400 * diff.r);
 
-        console.log(`Diff: ${diff.c}, ${diff.r}`);
-        
+        // console.log(`Diff: ${diff.c}, ${diff.r}`);
+
         // console.log(`Bg ${bgId} at ${bg.sprite.x}, ${bg.sprite.y}`);
-        
       }
     });
   }
@@ -216,7 +265,7 @@ enum Side {
   UP = 0,
   DOWN = 1,
   LEFT = 2,
-  RIGHT = 3
+  RIGHT = 3,
 }
 
 class Game {
@@ -225,6 +274,7 @@ class Game {
     "1_1",
     "1_2",
   ]);
+  uiManager: UIManager = new UIManager();
 
   setup() {
     this.spriteManager.add(new Player());
@@ -234,11 +284,14 @@ class Game {
     this.spriteManager.draw();
     this.backgroundManager.draw();
     drawSprites();
+    this.uiManager.draw()
     text(
       `${this.backgroundManager.pos.x} ${this.backgroundManager.pos.y}`,
       10,
       10
     );
+    const localPos = globalToLocal(this.backgroundManager.pos);
+    text(`${Math.round(localPos.x)} ${Math.round(localPos.y)}`, 10, 30);
   }
 }
 const game = new Game();
